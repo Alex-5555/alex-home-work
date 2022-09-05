41ea07082bb99a59c5c171be1e424381e977ae90
Alex <alan.saf@ukr.net>
2022-09-05 11:59:29 +0300

    alex

---
+++ test_9.py
@@ -0,0 +1,44 @@
+
+a = [8,3,5,7]
+b = [3,5,8]
+res = str([x for x in a if x >=1 and x<5])
+print(type(res))
+print('\'hello\'')
+
+# print(a[3])
+
+
+# a1 = set(a)
+# b1 = set(b)
+# print(a == b)
+
+
+# res = [x for x in b if x in a]
+# print(res) 
+
+# g = []
+# for i in b:
+#     if i in a:
+#         g.append(i)
+
+# num = []
+# for index in a + b:
+#     if index%2 == 0:
+#         print(index)
+
+
+
+
+
+
+
+# t = '4,6,8'
+
+
+# g = '2+6-3'
+
+
+    
+    
+    
+    
