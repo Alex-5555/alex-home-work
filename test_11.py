2a4f523ef8b7839f1b6994230ba3396c5ba10632
Alex <alan.saf@ukr.net>
2022-09-06 11:47:50 +0300

    Alex

---
+++ test_11.py
@@ -0,0 +1,16 @@
+g = '2+6-3'
+# g1 = list(g)
+# g1.remove('+')
+# g1.remove('-')
+# g2 = [int(x) for x in g1]
+# result = sum(g2[0:2])-g2[2]
+# print(result)
+print(eval(g))
+
+# g3 = sum(g2)
+# print(g3)
+
+
+
+# g_num = [x for x in g.split(',')]
+# print(g_num)
