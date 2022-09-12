409b6b03ccab8ff14a72bb819abbf7a0779880c9
Alex <alan.saf@ukr.net>
2022-09-12 12:24:02 +0300

    jkl

---
+++ test_14.py
@@ -0,0 +1,13 @@
+'''
+names = {"Alex,Dima,Olena"
+ages = "23,67,24"
+
+'''
+users = [
+    {"name": "Alex", "ages": "51"},
+    {"name": "Dima", "ages": "46"},
+    {"name": "Olena", "ages": "49"}   
+]
+
+
+
---
+++ test_15.py
@@ -0,0 +1,24 @@
+'''
+name? 1
+age? 2
+[{"name": "1", "age": "2"}]
+name? 3
+age? 4
+[{"name": "1", "age": "2"},{"name":"3", "age": "4"}]
+
+
+    {"name": "Alex", "ages": "51"},
+    {"name": "Dima", "ages": "46"},
+    {"name": "Olena", "ages": "49"} 
+'''
+
+users = []
+while True:
+    f = input('как тебя зовут ?  -  ')
+    g = input('сколько тебе лет ?  -  ')
+    user = {}
+    user["name"] = f
+    users.append(user)
+    #users.fromkeys(["name:", "ages:"], "f","g")
+    print(users)
+     
